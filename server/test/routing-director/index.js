3e0ed3bc34482c89521ec21ccdc6ab954d14831c
mue <mue.miv@gmail.com>
2015-06-19 08:46:50 +0300

    try to find solution for routing

---
+++ server/test/routing-director/index.js
@@ -0,0 +1,15 @@
+var director = require('director'),
+    router = new director.Router({
+        '/api' : function (arguments) {
+            console.log(arguments);
+            console.log(this);
+            console.log(123);
+        }
+    });
+
+router.dispatch('on', '/api', {
+    name: 123
+});
+
+// invoke
+
