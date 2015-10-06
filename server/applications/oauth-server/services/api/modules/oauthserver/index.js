aeebb1cca337fef45fb10db47437c669341b4294
mue <mue.miv@gmail.com>
2015-10-06 14:35:35 +0300

    improving api application 2

--- server/applications/oauth-server/services/api/modules/oauthserver/index.js
+++
@@ -1,7 +0,0 @@
-var oauthserver = require('common/modules/oauthserver'),
-    Application = require('common/resources/application'),
-    HttpError = require('common/errors/HttpError');
-
-var server = oauthserver.createServer();
-
-module.exports = server;
