db20bd25e4a7e83f7be62e628225dd7e6c2ad92d
mue <mue.miv@gmail.com>
2015-10-28 00:00:06 +0200

    improve post message

--- server/applications/proxy/services/web/public/js/postMessage.js
+++ server/applications/proxy/services/web/public/js/postMessage.js
@@ -1,19 +1,22 @@
 var response = response || {},
     meta = meta || {};
 
-function sendResponse(e){
+function sendResponse(e) {
     e.source.postMessage(JSON.stringify(response), "*");
-    setTimeout(function(){
+    setTimeout(function () {
         window.close();
     }, 100);
 }
 
 window.addEventListener('message', function (e) {
-    if(e.origin.indexOf('localhost') != -1){
+    // if it's localhost
+    if (e.origin.indexOf('localhost') != -1) {
         sendResponse(e);
-    }else{
-        if( meta.domain ){
-            if(e.origin == meta.domain){
+    } else {
+        if (meta.domain) {
+            if (meta.domain.indexOf('mue.in.ua') != -1) {
+                sendResponse(e);
+            } else if (e.origin == meta.domain) {
                 sendResponse(e);
             }
         }
